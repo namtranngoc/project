8f0a268b8a37b66c3386a4da302545e7750599a2
nam <llsakers2@gmail.com>
2025-11-12 17:34:43 +0700

    update

--- django_backend/myproject/settings.py
+++ django_backend/myproject/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASE_URL_RENDER = env('DATABASE_URL_RENDER', default=None)
 if DATABASE_URL_RENDER:
