e024c8af6fb8e7e21c58bee9d57addfe35732410
nam <llsakers2@gmail.com>
2025-11-12 18:20:42 +0700

    Fix: Add collectstatic to build script for Render

--- django_backend/build.sh
+++ django_backend/build.sh
@@ -2,5 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Thu thập tất cả các file tĩnh (CSS/JS) cần thiết cho trang Admin và các app
+python manage.py collectstatic --noinput
+
+# Chạy lệnh di trú database (migrate)
 python manage.py migrate
