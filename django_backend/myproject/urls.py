79d40001898ee1e934b91fb4d1f213fedc104d44
nam <llsakers2@gmail.com>
2025-11-14 16:17:37 +0700

    update

--- django_backend/myproject/urls.py
+++ django_backend/myproject/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from orders.views import OrderAdminViewSet
@@ -23,21 +7,24 @@ from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'orders/admin', OrderAdminViewSet, basename='admin-orders')
+
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
-    # 2. Thêm 2 dòng này để kích hoạt API của Djoser
+
+    # Djoser authentication
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
+
+    # API router
     path('api/', include(router.urls)),
-    path('admin/', admin.site.urls),
+
+    # App URLs
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
     path('users/', include('users.urls')),
-    path('api/', include('users.urls')),
 ]
 
-
+# Media files in DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
