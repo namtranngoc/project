b53e8c6f32b516569a9e563ae096dfcb93537a4d
Your Name <you@example.com>
2025-11-15 22:20:17 +0700

    fix api

--- django_backend/products/views.py
+++ django_backend/products/views.py
@@ -1,33 +1,16 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .models import Product
+from .serializers import ProductSerializer
 
-@csrf_exempt
-def create_product(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        price = request.POST.get('price')
-        image = request.FILES.get('image')
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-        if not all([name, price, image]):
-            return JsonResponse({'error':'Thiếu thông tin'}, status=400)
-
-        product = Product.objects.create(
-            name=name,
-            description=description,
-            price=price,
-            image=image
-        )
-        return JsonResponse({
-            'id': product.id,
-            'name': product.name,
-            'price': str(product.price),
-            'image': product.image.url
-        })
-    return JsonResponse({'error':'Method not allowed'}, status=405)
-
-def list_products(request):
-    products = Product.objects.all()
-    data = [{'id': p.id, 'name': p.name, 'price': str(p.price), 'image': p.image.url} for p in products]
-    return JsonResponse(data, safe=False)
+    # Phân quyền: Ai cũng được XEM, chỉ Admin mới được SỬA/XÓA
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
