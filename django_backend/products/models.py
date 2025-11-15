effbb985b9672a548c1a4a3944b42fd1ef58da84
Your Name <you@example.com>
2025-11-15 23:27:14 +0700

    fix api.

--- django_backend/products/models.py
+++ django_backend/products/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+    stock = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
